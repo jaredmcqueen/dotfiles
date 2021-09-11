8803a21608dd893fdc52d8dde2c68c39eb8d09a9
Jared McQueen <jaredmcqueen@gmail.com>
2021-09-11 02:23:46 +0000

    added dpi

--- scripts/.config/scripts/monitors.sh
+++ scripts/.config/scripts/monitors.sh
@@ -4,9 +4,11 @@ if [[ $(cat /etc/hostname) -eq "archlab" ]]
 then
   echo "this is the lab computer"
   sh ~/.config/scripts/monitors-dual.sh
+  xrdb -merge /$HOME/.config/scripts/highdpi
 fi
 
 if [[ $(cat /etc/hostname) -eq "archtop" ]]
 then
   echo "this is the laptop"
+  xrdb -merge /$HOME/.config/scripts/lowdpi
 fi
