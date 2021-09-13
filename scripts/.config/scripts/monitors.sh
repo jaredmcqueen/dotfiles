ea78d1d9545c2dd67364b9bdfe590f8a02eb5ca0
Jared McQueen <jaredmcqueen@gmail.com>
2021-09-13 08:42:54 -0400

    fixed dpi

--- scripts/.config/scripts/monitors.sh
+++ scripts/.config/scripts/monitors.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-if [[ $(cat /etc/hostname) -eq "archlab" ]]
-then
+if [ "$HOSTNAME" = "archlab" ]; then
   echo "this is the lab computer"
   sh ~/.config/scripts/monitors-dual.sh
   xrdb -merge /$HOME/.config/scripts/highdpi
 fi
 
-if [[ $(cat /etc/hostname) -eq "archtop" ]]
-then
+if [ "$HOSTNAME" = "archtop" ]; then
   echo "this is the laptop"
   xrdb -merge /$HOME/.config/scripts/lowdpi
 fi
